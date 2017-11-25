59b9f3add22a7b18707b39d01f3c690e9203f8df
tony <tony@omniaccount.com>
2017-11-25 13:17:50 +0800

    expose decode body

--- spec/reapal_helper.rb
+++ spec/reapal_helper.rb
@@ -4,7 +4,7 @@ require 'support/const_support'
 
 Faker::Config.locale = 'zh-CN'
 
-Reapal.logger = Logger.new('tmp/test.log')
+#Reapal.logger = Logger.new('tmp/test.log')
 
 RSpec.configure do |config|
   config.include ConstSupport
