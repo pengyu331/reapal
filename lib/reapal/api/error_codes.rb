27bfcfb783c5e3331c7a3b1724c9aad3f96745e4
chenpengyu <chenpengyu@omniaccount.com>
2017-11-26 23:49:37 +0800

    修改error_codes

--- lib/reapal/api/error_codes.rb
+++ lib/reapal/api/error_codes.rb
@@ -1,6 +1,44 @@
 module Reapal
-  module  Api
+  module Api
     module ErrorCode
+
+      def self.common
+        ['0001']
+      end
+
+      def self.contract_common
+        ['0103']
+      end
+
+      def self.bind_card
+        @_bind_card ||= %w(9060 9065 9083 9084 9085 0017 0210 0302 0303 0328 0329
+                           0330 0332 0333 0335 0339 0340 0341 0342 0343 0344 0345
+                           0346 0347 0348 0350 0351 0352 0358 0359 0360 0362 0363)
+      end
+
+      def self.deposit
+        @_deposit ||= %w(0000 0001 0003 0004 0005 0201 0202 0203 0204 0205 0206
+                         0207 0208 0209 1701 1702 1703 1704 1705 1706 1707 1708
+                         1709 1710 1711 1712 1713 1714 1801 1802 1803 1804 1805
+                         1901)
+      end
+
+      def self.sub_account
+        @_sub_account ||= %w(0001)
+      end
+
+      def self.deposit_common
+        @_deposit_common ||= %w(0001 0004)
+      end
+
+      def self.deposit_apply_api
+        @_deposit_apply_api ||= %w(1701 1702 1703 1704 1705 1706 1707 1708 1709 1710 1711
+                         1712 1713 1714) | deposit_common
+      end
+
+      def self.deposit_confirm_api
+        @_deposit_confirm_api ||= %w(1901) | deposit_common
+      end
 
       def tender_common
         @_tender_common = %w(0400 0401 0402 0405 0406 0407 0408 0409 0410)
