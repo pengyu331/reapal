207d405f6849a91b414ce1282b59943da4b1ee92
peterswj <peterswj@163.com>
2017-11-28 02:13:39 +0800

    一键转债spec

--- spec/api/asset/tender_onekey_single_transfer_spec.rb
+++ spec/api/asset/tender_onekey_single_transfer_spec.rb
@@ -3,6 +3,72 @@ require 'reapal_helper'
 
 RSpec.describe '一键债权转让' do
 
-  it '成功'
+  it '首次债转,成功' do
+    # 1. 发标
+    tender_no = borrower_tender_apply_flow_id_02
+    # 2. 一键投标
+
+    result = client.tender_onekey_invest(Reapal::Utils.gen_flow_id,
+                        tender_no,
+                        100,
+                        nil,
+                        investor_contract_01)
+    # 3.一键债转
+    result = client.tender_onekey_single_transfer(Reapal::Utils.gen_flow_id,
+                                                  tender_no,
+                                                  100,
+                                                  100,
+                                                  investor_contract_01,
+                                                  investor_contract,
+                                                  1,
+                                                  result[:data][:orderNo],
+                                                  ''
+                                                  )
+
+    expect(result[:result]).to eq('S')
+
+  end
+
+
+  it '二次债转,成功' do
+    # 1. 发标
+    tender_no = borrower_tender_apply_flow_id_02
+    # 2. 一键投标
+
+    result = client.tender_onekey_invest(Reapal::Utils.gen_flow_id,
+                        tender_no,
+                        100,
+                        nil,
+                        investor_contract_01)
+    # 3.一键债转
+    result0 = client.tender_onekey_single_transfer(Reapal::Utils.gen_flow_id,
+                                                  tender_no,
+                                                  100,
+                                                  100,
+                                                  investor_contract_01,
+                                                  investor_contract,
+                                                  1,
+                                                  result[:data][:orderNo],
+                                                  ''
+                                                  )
+
+    result = client.tender_onekey_single_transfer(Reapal::Utils.gen_flow_id,
+                                                  tender_no,
+                                                  100,
+                                                  100,
+                                                  investor_contract,
+                                                  investor_contract_02,
+                                                  2,
+                                                  '',
+                                                  result0[:data][:orderNo]
+                                                  )
+
+
+
+
+
+    expect(result[:result]).to eq('S')
+
+  end
 
 end
