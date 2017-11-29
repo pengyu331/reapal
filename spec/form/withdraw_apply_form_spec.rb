54d90807416d60d4af8fb5b7d21eb81f47fdda6a
tony <tony@omniaccount.com>
2017-11-29 11:55:53 +0800

    update

--- spec/form/withdraw_apply_form_spec.rb
+++ spec/form/withdraw_apply_form_spec.rb
@@ -4,13 +4,13 @@ require 'reapal_helper'
 RSpec.describe '提现申请' do
   it '成功' do
     sleep(3) # 否则会报“签约过于频繁”的错误
-    new_phone = Faker::PhoneNumber.cell_phone
     result = client.withdraw_apply_form(test_contracts, Reapal::Utils.gen_flow_id, 100, 1, '', '')
 
+    method = result[:form_method]
     result = result[:form_data]
 
     html = <<-EOF
-<form action="#{result[:url]}" method="#{result[:method]}">
+<form action="#{method[:url]}" method="#{method[:method]}">
   <p>First name: <input type="text" name="merchant_id" value="#{result[:merchant_id]}"/></p>
   <p>Last name: <input type="text" name="encryptkey" value="#{result[:encryptkey]}"/></p>
   <p>Last name: <input type="text" name="data" value="#{result[:data]}"/></p>  
