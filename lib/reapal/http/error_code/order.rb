6ade0290dfce14d5792ab4072324e34055dc6fa6
chenpengyu <chpy15@gmail.com>
2017-12-09 16:08:27 +0800

    去除结果为pending的errorCode

--- lib/reapal/http/error_code/order.rb
+++ lib/reapal/http/error_code/order.rb
@@ -5,11 +5,11 @@ module Reapal
     module ErrorCode
 
       def self.query_by_single_flow_id
-        @_query_by_single_flow_id ||= %w(1100 1101 1102 1103 1104 1105 1106 1107 1108 1109 0001) | tender_common
+        @_query_by_single_flow_id ||= %w(1103 1104 1105 1106 1107 1108 1109 0001 0519) | tender_common
       end
 
       def self.query_by_flow_id
-        @_query_by_flow_id ||= %w(1100 1101 1102 1103 1104 1105 1106 1107 1108 1109 0001) | tender_common
+        @_query_by_flow_id ||= %w(1103 1104 1105 1106 1107 1108 1109 0001) | tender_common
       end
 
     end # module ErrorCode
--- lib/reapal/http/error_code/user.rb
+++ lib/reapal/http/error_code/user.rb
@@ -5,24 +5,23 @@ module Reapal
     module ErrorCode
 
       def self.contract_common
-        ['0103'] | common
+        @_contract_common ||= %w(0101 0102 0103 0104 0105 0106 0107 0108 0109 0110
+                                 0111 0112 0113 0114 0115 0116 0117 0118 0119 0120
+                                 0121 0122 0123 0124 0125 0126 0127 0128 0129 0130
+                                 0131 0132 0150 0151 0152 0153 0154 0155 0156 0157
+                                 0158 0159 0160 0161 0162 0163 0164 0165 0166 0167) | common
       end
 
       def self.contract_create
-        ['0101', '0102', '0103', '0104', '0105', '0106', '0107', '0108', '0109',
-         '0110', '0111', '0112', '0113', '0114', '0115', '0116', '0117', '0118',
-         '0119', '0120', '0121', '0122', '0123', '0124', '0125', '0126', '0127',
-         '0128', '0129', '0130', '0131', '0132', '0150', '0151', '0152', '0153',
-         '0154', '0155', '0156', '0157', '0158', '0159', '0160', '0161', '0162',
-         '0163', '0164', '0165', '0166', '0167', '0525'] | contract_common
+        @_contract_create | contract_common
       end
 
       def self.com_contract_create
-        ['9015'] | contract_create | contract_common
+        @_com_contract_common ||= ['9015'] | contract_common
       end
 
       def self.contract_query
-        ['0113'] | contract_common
+        @_contract_query | contract_common
       end
 
       def self.mobile
@@ -30,10 +29,10 @@ module Reapal
       end
 
       def self.bind_card
-        @_bind_card ||= %w(9060 9064 9065 9083 9084 9085 0017 0210 0302 0303 0328
+        @_bind_card ||= %w(9060 9064 9065 9083 9084 9085 0017 0302 0303 0328
                            0329 0330 0332 0333 0335 0339 0340 0341 0342 0343
-                           0344 0345 0346 0347 0348 0350 0351 0352 0358 0359
-                           0360 0362 0363) | contract_common
+                           0345 0346 0347 0348 0350 0351 0352 0358 0359 0360
+                           0362 0363) | contract_common
       end
 
     end # module ErrorCode
