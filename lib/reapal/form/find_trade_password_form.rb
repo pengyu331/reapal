816b398efd41c2b9e89b994e13c7a1d69dab9ae0
chenpengyu <chpy15@gmail.com>
2018-01-26 18:52:01 +0800

    修改设置交易密码 form

--- lib/reapal/form/find_trade_password_form.rb
+++ lib/reapal/form/find_trade_password_form.rb
@@ -4,12 +4,13 @@ module Reapal
   module Form
     module FindTradePassowrdForm
 
-      # 1.9 设置/修改交易密码
+      # 1.5 设置/修改交易密码
       #
       # @param contract [String] 用户协议
       # @param return_url [String] 回调 url
       # @param notify_url [String] 通知 url
       # @param busway [String] 设备通道，默认手机端。00：PC端；01：手机端；02：Pad端；03：其它
+      # @param version [String] 版本号默认1.0，迁移历史用户 version 值传3.0
       #
       # @return [ Hash ] 结果集
       #   * :form_method
@@ -20,7 +21,7 @@ module Reapal
       #     * :encryptkey
       #     * :data
       #
-      def find_trade_password_form(contract, return_url, notify_url, busway='01')
+      def find_trade_password_form(contract, return_url, notify_url, busway='01', version='1.0')
         service = 'reapal.trust.findTradePassword'
         post_path = '/reagw/findTradePassword/findTradePassword.htm'
 
@@ -32,7 +33,7 @@ module Reapal
           applyTime: Time.now.strftime('%Y-%m-%d %H:%M:%S'),
         }
 
-        get_form_data(service, params, post_path)
+        get_form_data(service, params, post_path, version)
       end
 
     end
