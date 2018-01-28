8d6b725453394bb5fb8e9a859b9384df0837579a
chenpengyu <chpy15@gmail.com>
2018-01-29 01:48:59 +0800

    升级网银充值form

--- lib/reapal/form/deposit_apply_form.rb
+++ lib/reapal/form/deposit_apply_form.rb
@@ -14,7 +14,6 @@ module Reapal
       # @param notify_url [String] 通知 url
       # @param remark [String] 备注(默认为 '')
       # @param payment_type [String]（默认为 1）1：表示富民银行收银台支付（默认值，此字段为空时，默认此种方式）；2：表示B2B企业充值银行直连；3： 表示B2C个人借记卡银行直连；
-      # @param channel [String] (默认为 bank) 如果是跳转收银台值为：bank，银行直连请参考：银行代码
       # @param pay_customer_no [String] (默认为 '')支付方式（paymentType参数）为企业银行直连时：民生、浦发、交通三家银行 B2B(企业网银)支付需要提交该字段。其他情况下提交空字符串
       #
       # @return [ Hash ] 结果集
@@ -28,7 +27,7 @@ module Reapal
       #
       def deposit_apply_form(flow_id, contracts, money, charge,
                              return_url, notify_url, remark='',
-                             payment_type='1', channel='bank', pay_customer_no='')
+                             payment_type='1', pay_customer_no='')
 
         service = 'reapal.trust.depositApply'
         post_path = '/reagw/service/deposit.htm'
@@ -42,13 +41,13 @@ module Reapal
           notifyUrl: notify_url,
           remark: remark,
           paymentType: payment_type,
-          channel: channel,
+          channel: 'bank',
           payCustomerNo: pay_customer_no,
           busway: '00',
           applyTime: Time.now.strftime('%Y-%m-%d %H:%M:%S')
         }
 
-        get_form_data(service, params, post_path)
+        get_form_data(service, params, post_path, '3.0')
       end
 
     end #BusinessAuth
