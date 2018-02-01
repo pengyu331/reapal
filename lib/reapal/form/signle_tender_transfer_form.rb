25b50308ea603fc1ab695e7f49248e6b2922c132
chenpengyu <chpy15@gmail.com>
2018-02-02 04:27:05 +0800

    修改债权转让文档

--- lib/reapal/form/signle_tender_transfer_form.rb
+++ lib/reapal/form/signle_tender_transfer_form.rb
@@ -13,8 +13,8 @@ module Reapal
       # @param trans_contracts [String] 债权转让方在存管系统签约的协议号
       # @param into_contracts [String] 债权承接方在存管系统签约的协议号
       # @param transfer_type [String] 债转类型,传1表示首次债转，即转让人（transContracts）为tenderNo指代标的原始投资人,传2表示第N次债转,转让人（transContracts）并非原标的投资人
-      # @param invest_order [String] 若transferType为1时,即首次债转时必传
-      # @param org_transfer_order [String] 若transferType 为2时,即N次债转时必传
+      # @param invest_order [String] 原投资订单号,若transferType为1时,即首次债转时必传
+      # @param org_transfer_order [String] 原债转订单号,若transferType 为2时,即N次债转时必传
       # @param return_url [String] 回调 url
       # @param notify_url [String] 通知 url
       # @param busway [String] 00：PC端；01：手机端；02：Pad端；03：其它
