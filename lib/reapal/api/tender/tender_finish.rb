a8826b1ffc47c508cf1ec39dff1e01711a1a56f2
chenpengyu <chpy15@gmail.com>
2018-02-02 03:36:04 +0800

    修改满标接口

--- lib/reapal/api/tender/tender_finish.rb
+++ lib/reapal/api/tender/tender_finish.rb
@@ -10,15 +10,15 @@ module Reapal
         # @param flow_id [ String ] 完成订单号
         # @param tender_no [ String ] 商户系统标的号
         # @param debit_contracts [ String ] 借款方协议号
+        # @param invest_details [ Array ] 投资方分账
+        #   * :seriaNo [String] 商户流水号
+        #   * :investContracts [String] 投资方协议号
+        #   * :amount [BigDecimal] 金额
+        #   * :remark [String] 备注
         # @param fee_details [ Array ] 手续费分账
         #   * :seriaNo [String] 商户流水号
         #   * :payeeContracts [String] 收款方协议号
         #   * :feeType [String] 01 收借款人手续费【包括平台和担保方，共享发标时的手续费】 02 受托支付
-        #   * :amount [BigDecimal] 金额
-        #   * :remark [String] 备注
-        # @param invest_details [ Array ] 投资方分账
-        #   * :seriaNo [String] 商户流水号
-        #   * :investContracts [String] 投资方协议号
         #   * :amount [BigDecimal] 金额
         #   * :remark [String] 备注
         # @param busway [ String ] 设备通道， '00'：PC端；'01'：手机端(默认)；'02'：Pad端；'03'：其它
@@ -34,8 +34,9 @@ module Reapal
         #      * :orderNo [String] 完成订单号
         #      * :resultCode [String] 结果代码
         #
-        def tender_finish(flow_id, tender_no, debit_contracts, fee_details,
-                          invest_details, busway='01', remark='')
+        def tender_finish(flow_id, tender_no, debit_contracts, 
+                          invest_details, fee_details=[], 
+                          busway='01', remark='')
           service = 'reapal.trust.tenderFinish'
           post_path = '/reagw/tender/rest.htm'
 
--- spec/api/tender/tender_finish_spec.rb
+++ spec/api/tender/tender_finish_spec.rb
@@ -3,47 +3,6 @@ require 'reapal_helper'
 
 RSpec.describe '满标' do
 
-  # it '生成标的' do
-  #   tender_141
-  # end
-
-  # it '用户投标' do
-  #   tender_no = '5a71f90ecd5dbb21a8000002'
-  #   result = client.tender_onekey_invest(Reapal::Utils.gen_flow_id,
-  #                                          tender_no,
-  #                                          100,
-  #                                          nil,
-  #                                          invester_001[:contract])
-
-  #   expect(result[:result]).to eq("S")
-  # end
-
-  it '满标' do
-    fee_details = [{
-      serialNo: Reapal::Utils.gen_flow_id,
-      payeeContracts: 'R100000000532891',
-      amount: 0.1,
-      feeType: '01',
-      remark: ''
-      }]
-  
-    invest_details = [
-      { serialNo: Reapal::Utils.gen_flow_id,
-        investContracts: invester_001[:contract],
-        amount: 100,
-        remark: '' } ]
-  
-    result = client.tender_finish(Reapal::Utils.gen_flow_id,
-                                  '5a71f90ecd5dbb21a8000002',
-                                  borrower_141[:contract],
-                                  fee_details,
-                                  invest_details)
-  
-  
-    expect(result[:result]).to eq("S")
-    expect(result[:data][:resultCode]).to eq('0000')
-  end
-
   # it "成功" do
   #   #1.发标
   #     #A发标 100元
@@ -80,10 +39,65 @@ RSpec.describe '满标' do
   #                                 borrower_contract,
   #                                 debit_details,
   #                                 invest_details)
-  
-  
+
+
   #   expect(result[:result]).to eq("S")
   #   expect(result[:data][:resultCode]).to eq('0000')
   # end
 
+  it '满标' do
+    fee_details = [{
+      serialNo: Reapal::Utils.gen_flow_id,
+      payeeContracts: 'R100000000532891',
+      amount: 0.0,
+      feeType: '01',
+      remark: ''
+      }]
+  
+    invest_details = [
+      { serialNo: Reapal::Utils.gen_flow_id,
+        investContracts: invester_001[:contract],
+        amount: 100,
+        remark: '' } ]
+  
+    result = client.tender_finish(Reapal::Utils.gen_flow_id,
+                                  '5a71f90ecd5dbb21a8000002',
+                                  borrower_141[:contract],
+                                  fee_details,
+                                  invest_details)
+  
+  
+    expect(result[:result]).to eq("S")
+    expect(result[:data][:resultCode]).to eq('0000')
+  end
+
+  it '借款141的标的满标' do
+    serial_no1 = Reapal::Utils.gen_flow_id
+    serial_no2 = Reapal::Utils.gen_flow_id
+
+    invest_details = [
+      { serialNo: serial_no1,
+        investContracts: invester_001[:contract],
+        amount: 500,
+        remark: '' },
+      { serialNo: serial_no2,
+        investContracts: invester_002[:contract],
+        amount: 500,
+        remark: '' },
+    ]
+
+    flow_id = Reapal::Utils.gen_flow_id
+
+    result = client.tender_finish(flow_id,
+                                  '5a7353eacd5dbb5908000002',
+                                  borrower_141[:contract],
+                                  invest_details)
+    
+    puts "serial_no1: #{serial_no1}"
+    puts "serial_no2: #{serial_no2}"
+    puts "flow_id: #{flow_id}"
+    puts result[:data]
+    expect(result[:result]).to eq("S")
+    expect(result[:data][:resultCode]).to eq('0000')
+  end
 end
