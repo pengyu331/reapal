0ea2d96746f84bc7564b57eb1bd851ce49fc2920
chenpengyu <chpy15@gmail.com>
2018-02-02 09:39:32 +0800

    测试债权转让和投标

---
+++ spec/form/signle_tender_transfer_form_spec.rb
@@ -0,0 +1,42 @@
+# coding: utf-8
+require 'reapal_helper'
+
+RSpec.describe '债权转让' do
+  let(:tender_no) { '5a7353eacd5dbb5908000002' }
+
+  it '成功' do
+    flow_id = Reapal::Utils.gen_flow_id
+    result = client.single_tender_transfer_form(
+      flow_id,
+      tender_no,
+      100,
+      100.1,
+      invester_002[:contract],
+      invester_001[:contract],
+      '1',
+      '5a7361cbcd5dbbc417000001',
+      nil,
+      'http://127.0.0.1',
+      'http://127.0.0.1'
+    )
+
+    method = result[:form_method]
+    result = result[:form_data]
+
+    html = <<-EOF
+<form action="#{method[:url]}" method="#{method[:method]}">
+  <p>First name: <input type="text" name="merchant_id" value="#{result[:merchant_id]}"/></p>
+  <p>Last name: <input type="text" name="encryptkey" value="#{result[:encryptkey]}"/></p>
+  <p>Last name: <input type="text" name="data" value="#{result[:data]}"/></p>  
+  <input type="submit" value="Submit" />
+</form>
+    EOF
+
+    path = "tmp/spec_signle_tender_transfer_form.html"
+    fp = File.open(path, "w+")
+    fp.write html
+    fp.close
+    puts "flow_id: #{flow_id}"
+    puts "测试 html 导入到：#{path}"
+  end
+end
