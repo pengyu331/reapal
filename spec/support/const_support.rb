4843f45954bc08bc4409763408b67a95e3a38ccf
chenpengyu <chpy15@gmail.com>
2018-03-02 21:15:56 +0800

    增加 config

--- spec/support/const_support.rb
+++ spec/support/const_support.rb
@@ -7,16 +7,12 @@ require 'yaml'
 module ConstSupport
 
   def client
-    config = YAML.load_file(File.join(__dir__, 'config/config.yml'))
+    config = YAML.load_file(File.join(__dir__, 'config/config.develop.yml'))
+
+    config = Reapal::Utils.symbolize_keys(config)
 
     @client ||= Reapal::Client.new(
-      server_url: config[:server_url.to_s],
-      partner_id: config[:partner_id.to_s],
-      merchant_id: config[:merchant_id.to_s],
-      md5_key: config[:md5_key.to_s],
-      public_key: config[:public_key.to_s],
-      private_key: config[:private_key.to_s],
-      private_key_pwd: config[:private_key_pwd.to_s],
+      config
     )
   end
 
