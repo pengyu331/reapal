6901ee0e235a143211c04609dc3f0e0dde580148
chenpengyu <chpy15@gmail.com>
2018-03-19 18:05:04 +0800

    增加标的迁移 spec

---
+++ spec/api/tender/tender_history_apply_spec.rb
@@ -0,0 +1,32 @@
+# coding: utf-8
+require 'reapal_helper'
+
+RSpec.describe '发标' do
+  context 'version3.0' do
+    let(:tender_no) { Reapal::Utils.gen_flow_id }
+    let(:name) { '个人借款迁移' }
+    let(:contract) { 'RB180302QP1JJUEJ' }
+
+    it '迁移历史标的' do
+      flow_id = Reapal::Utils.gen_flow_id
+      result = client.tender_history_apply(Reapal::Utils.gen_flow_id,
+                                  tender_no,
+                                  name,
+                                  2000,
+                                  1,
+                                  0,
+                                  '1',
+                                  1,
+                                  '2',
+                                  Time.now + 24 * 3600,
+                                  Time.now + 3600 * 24,
+                                  '01',
+                                  contract)
+
+      puts result[:data]
+      puts "flow_id： #{flow_id}"
+
+      expect(result[:result]).to eq 'S'
+    end
+  end
+end
